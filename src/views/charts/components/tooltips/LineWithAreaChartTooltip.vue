4e98483920d7d69fe903470efd2a8196a7bd0489
kove.vit@gmail.com <kove.vit@gmail.com>
2024-08-27 01:39:33 +0300

    v7

---
+++ src/views/charts/components/tooltips/LineWithAreaChartTooltip.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hint">
+    <div class="hint__title mb-3">{{ title }}</div>
+    <div class="d-flex justify-space-between">
+      <span class="hint__range">TODO_Средний:</span>
+      <span class="hint__range-count">{{ count }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    count: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hint {
+  min-width: 308px;
+  // min-height: 60px;
+}
+
+.hint__title {
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 16px;
+  /* identical to box height, or 100% */
+
+  /* Black */
+  color: #222529;
+}
+
+.hint__range {
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 100%;
+  /* identical to box height, or 14px */
+
+  /* Black */
+  color: #222529;
+
+  opacity: 0.7;
+}
+
+.hint__range-count {
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 100%;
+
+  /* Black */
+  color: #222529;
+}
+</style>
--- src/views/charts/composable/colorsMapping.js
+++ src/views/charts/composable/colorsMapping.js
@@ -1,40 +1,40 @@
 export const COLOR_MAPPING = {
-  GREEN_DONAT: '#48CD9D',
-  YELLOW_DONAT: '#FFD466',
-  ORANGE_DONAT: '#FFA740',
-  RED_DONAT: '#FF764C',
-  GREY_DONAT: '#F3F6F9',
+  GREEN: '#48CD9D',
+  YELLOW: '#FFD466',
+  ORANGE: '#FFA740',
+  RED: '#FF764C',
+  GREY: '#F3F6F9',
 };
 
 export const colorMapDonat = {
-  empty: COLOR_MAPPING.GREY_DONAT,
+  empty: COLOR_MAPPING.GREY,
 
-  positive_answer: COLOR_MAPPING.GREEN_DONAT,
-  loyal_answer: COLOR_MAPPING.YELLOW_DONAT,
-  skeptic_answer: COLOR_MAPPING.ORANGE_DONAT,
-  negative_answer: COLOR_MAPPING.RED_DONAT,
+  positive_answer: COLOR_MAPPING.GREEN,
+  loyal_answer: COLOR_MAPPING.YELLOW,
+  skeptic_answer: COLOR_MAPPING.ORANGE,
+  negative_answer: COLOR_MAPPING.RED,
 
-  supporters: COLOR_MAPPING.GREEN_DONAT,
-  loyal: COLOR_MAPPING.YELLOW_DONAT,
-  skeptics: COLOR_MAPPING.ORANGE_DONAT,
-  critics: COLOR_MAPPING.RED_DONAT,
+  supporters: COLOR_MAPPING.GREEN,
+  loyal: COLOR_MAPPING.YELLOW,
+  skeptics: COLOR_MAPPING.ORANGE,
+  critics: COLOR_MAPPING.RED,
 
-  extremely_low_expression: COLOR_MAPPING.GREEN_DONAT,
-  low_expression: COLOR_MAPPING.YELLOW_DONAT,
-  medium_expression: COLOR_MAPPING.ORANGE_DONAT,
-  high_expression: COLOR_MAPPING.RED_DONAT,
+  extremely_low_expression: COLOR_MAPPING.GREEN,
+  low_expression: COLOR_MAPPING.YELLOW,
+  medium_expression: COLOR_MAPPING.ORANGE,
+  high_expression: COLOR_MAPPING.RED,
 
   emotionalDeperson: {
-    extremely_high_expression: COLOR_MAPPING.RED_DONAT,
-    low_expression: COLOR_MAPPING.GREEN_DONAT,
-    medium_expression: COLOR_MAPPING.YELLOW_DONAT,
-    high_expression: COLOR_MAPPING.ORANGE_DONAT,
+    extremely_high_expression: COLOR_MAPPING.RED,
+    low_expression: COLOR_MAPPING.GREEN,
+    medium_expression: COLOR_MAPPING.YELLOW,
+    high_expression: COLOR_MAPPING.ORANGE,
   },
 
   efficiency: {
-    extremely_high_prof_efficiency: COLOR_MAPPING.GREEN_DONAT,
-    high_prof_efficiency: COLOR_MAPPING.YELLOW_DONAT,
-    medium_prof_efficiency: COLOR_MAPPING.ORANGE_DONAT,
-    low_prof_efficiency: COLOR_MAPPING.RED_DONAT,
+    extremely_high_prof_efficiency: COLOR_MAPPING.GREEN,
+    high_prof_efficiency: COLOR_MAPPING.YELLOW,
+    medium_prof_efficiency: COLOR_MAPPING.ORANGE,
+    low_prof_efficiency: COLOR_MAPPING.RED,
   },
 };
